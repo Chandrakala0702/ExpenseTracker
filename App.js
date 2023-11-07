58ac19618077a65b343456f0417eb77ad58c7f19
Chandrakala72 <chandrakala@technerdssolutions.com>
2023-11-07 17:31:43 +0530

    Added splash screen and dashboard view changes

--- App.js
+++ App.js
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import LoginScreen from "./src/views/login";
-import DashboardScreen from './src/views/dashboard';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import DashboardScreen from "./src/views/dashboard";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import store from "./src/redux/store/store";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import SplashScreen from "react-native-splash-screen";
 
-const AppNavigator = createStackNavigator({
-  Login: {
-    screen: LoginScreen,
-    navigationOptions: {
-      title: "Expense Tracker Login",
+const AppNavigator = createStackNavigator(
+  {
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: {
+        title: "Expense Tracker Login",
+      },
+    },
+    Dashboard: {
+      screen: DashboardScreen,
+      navigationOptions: {
+        title: "Expense Tracker Dashboard",
+        headerShown: false,
+      },
     },
   },
-  Dashboard: {
-    screen: DashboardScreen, // Add your Dashboard screen here
-    navigationOptions: {
-      title: 'Expense Tracker Dashboard',
-      headerShown: false,
-    },
-  }},
   {
-    initialRouteName: 'Login', // Set the initial route to the Login screen
+    initialRouteName: "Login",
   }
 );
 
 const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
-    <SafeAreaProvider>
-      <AppContainer />
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+        <AppContainer />
+      </SafeAreaProvider>
     </Provider>
   );
 }
